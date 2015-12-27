27b09401e67a90ba241c326624694adb24c3877d
clover <clover@solidyear.com.tw>
2015-12-27 23:46:10 +0800

    form class view have some problem when I practice

--- classview/urls.py
+++ classview/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from myview.views import GreetingView, MorningGreetingView
+from myview.views import GreetingView, MorningGreetingView, success
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^about/', GreetingView.as_view()),
+    # url(r'^about/', GreetingView.as_view()),
     url(r'^aboutM/$', MorningGreetingView.as_view()),
+    url(r'^success/$', success),
 ]
--- myview/views.py
+++ myview/views.py
@@ -4,6 +4,12 @@
 from django.http import HttpResponse
 from django.views.generic import View
 
+# Handling forms with class-based views
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django import forms
+from .forms import MyForm
+
 
 class GreetingView(View):
     greeting = "Good Day"
@@ -14,3 +20,37 @@ class GreetingView(View):
 
 class MorningGreetingView(GreetingView):
     greeting = "Morning to Ya"
+
+
+def myview(request):
+    if request.method == "POST":
+        form = MyForm(request.POST)
+        if form.is_valid():
+            # xxxxxx
+            return HttpResponseRedirect('/success/')
+    else:
+        form = MyForm(initial={'key': 'value'})
+
+    return render(request, 'form_template.html', {'form': form})
+
+
+class MyFormView(View):
+    form_class = MyForm
+    initial = {'key': 'value'}
+    template_name = 'form_template.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            # process form cleaned data
+            return HttpResponseRedirect('/success/')
+
+        return render(request, self.template_name, {'form': form})
+
+
+def success(request):
+    return HttpResponse("Success !")
